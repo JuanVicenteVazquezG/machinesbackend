353c56deb0776047b33f6649e434ccca6617354b
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-11-12 21:04:35 +0100

    add first routes in routes.yaml

--- src/Controller/MachinesController.php
+++ src/Controller/MachinesController.php
@@ -3,15 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class MachinesController extends AbstractController
 {
-    /**
-     * @Route("/machines", name="machines")
-     */
-    public function index(): Response
+    public function index()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+
+// use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user")
-     */
-    public function index(): Response
+    public function index()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
