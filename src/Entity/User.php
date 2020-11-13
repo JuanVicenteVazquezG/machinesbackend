4540f62a4ebfcfec37818985f3d71bfc3e22a1ea
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-11-13 13:02:00 +0100

    Acces method Post

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -130,7 +130,6 @@ class User implements \JsonSerializable
             'name' => $this->name,
             'surname'=> $this->surname,
             'email'=> $this->email,
-            'password' => $this->password,
-        ];
+                    ];
     }
 }
