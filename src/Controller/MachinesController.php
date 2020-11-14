e50a3d64dc2478c7d51202be872a35d2b3b3559d
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-11-14 08:24:17 +0100

    New Machine

--- src/Controller/MachinesController.php
+++ src/Controller/MachinesController.php
@@ -3,14 +3,124 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints\Email;
+
+
+use App\Entity\User;
+use App\Entity\Machine;
+use App\Services\JwtAuth;
 
 class MachinesController extends AbstractController
 {
+    private function resjson($data)
+    {
+        // Serialize data with service Serializer
+        $json = $this->get('serializer')->serialize($data, 'json');
+        // Response with httpFoundation
+        $response = new Response();
+        // Assign content to the response
+        $response->setContent($json);
+        // Indicate response format json
+        $response->headers->set('Content-Type', 'application/json');
+        // return response
+        return $response;
+    }
+    
     public function index()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/MachinesController.php',
         ]);
+    }
+
+    public function newMachine(Request $request, JwtAuth $jwt_auth)
+    {
+        $data = [
+            'status'=>'error',
+            'code'=>400,
+            'message'=>'A new Machine could not be created!'
+        ];
+
+        //get the token
+
+        $token = $request->headers->get('Authorization', null);
+
+        // check if is correct the token
+
+        $authCheck = $jwt_auth->checkToken($token);
+
+
+        if ($authCheck) {
+            
+        // get data by post
+
+            $json = $request->get('json', null);
+
+            $params = json_decode($json);
+            // get identified user object
+
+            $identity = $jwt_auth->checkToken($token, true);
+
+            // check and validate data
+
+            if (!empty($json)) {
+                $user_id = ($identity->sub !=null) ? $identity->sub : null;
+                $brand = (!empty($params->brand)) ? $params->brand : null;
+                $model = (!empty($params->model)) ? $params->model : null;
+                $manufacturer = (!empty($params->manufacturer)) ? $params->manufacturer : null;
+                $price = (!empty($params->price)) ? $params->price : null;
+                $image_front_url = (!empty($params->image_front_url)) ? $params->image_front_url : null;
+                $image_lateral_url = (!empty($params->image_lateral_url)) ? $params->image_lateral_url : null;
+                $image_thumbnail_url = (!empty($params->image_thumbnail_url)) ? $params->image_thumbnail_url : null;
+               
+                if (!empty($user_id) && !empty($brand) && !empty($model) && !empty($manufacturer)) {
+                    // Save the new Machine in database
+                  
+                    $em = $this->getDoctrine()->getManager();
+                    $user = $this->getDoctrine()->getRepository(User::class)->findOneBy([
+                'id'=> $user_id
+                ]);
+
+                    //Create and Save object
+                    $machine = new Machine();
+                    $machine->setUser($user);
+                    $machine->setBrand($brand);
+                    $machine->setModel($model);
+                    $machine->setManufacturer($manufacturer);
+                    $machine->setPrice($price);
+                    $machine->setImageFrontUrl($image_front_url);
+                    $machine->setImageLateralUrl($image_lateral_url);
+                    $machine->setImageThumbnailUrl($image_thumbnail_url);
+                    $createdAt = new \DateTime('now');
+                    $updatedAt = new \DateTime('now');
+                    $machine->setCreatedAt($createdAt);
+                    $machine->setUpdatedAt($updatedAt);
+                    // Save the new Machine in database
+                    $em->persist($machine);
+                    $em->flush();
+
+                    $data=[
+                        'status'=>'success',
+                        'code'=>200,
+                        'message'=>'The new machine was saved!',
+                        'machine'=>$machine
+                    ];
+                }
+            }
+            return $this->resjson($data);
+        }
+
+        
+
+
+
+
+       
+        return $this->resjson($data);
     }
 }
